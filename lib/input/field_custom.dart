2c37783a93847f0a7841958035c78e1f1d8fe529
andyguzman7 <andyguzman117gmail.com>
2024-09-05 18:21:20 -0400

    details input

--- lib/input/field_custom.dart
+++ lib/input/field_custom.dart
@@ -9,6 +9,8 @@ class FieldCustom extends StatelessWidget {
     required this.onFocusChange,
     required this.label,
     this.isLoading = false,
+    this.isLabelTitle = false,
+    this.showDecoration = false,
     super.key,
   });
   final ControllerField controller;
@@ -17,58 +19,69 @@ class FieldCustom extends StatelessWidget {
   final void Function(bool) onFocusChange;
   final bool isLoading;
   final String label;
+  final bool isLabelTitle;
+  final bool showDecoration;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(left: 10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8),
-        boxShadow: const [
-          BoxShadow(
-            blurRadius: 16,
-            color: Colors.black12,
-            offset: Offset(0, 4),
-          ),
+    return Column(
+      children: [
+        if (isLabelTitle) ...[
+          Text(label),
+          const SizedBox(height: 5),
         ],
-      ),
-      child: FocusScope(
-        onFocusChange: onFocusChange,
-        child: TextFormField(
-          key: controller.fieldKey,
-          validator: validator,
-          readOnly: isLoading,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          controller: controller.textEditingController,
-          onChanged: onChanged,
-          decoration: InputDecoration(
-            isDense: true,
-            hintText: label,
-            hintStyle: TextStyle(
-              color: Colors.grey.shade400,
-            ),
-            errorText: null,
-            errorStyle: const TextStyle(
-              color: Colors.transparent,
-              fontSize: 0,
-            ),
-            focusedErrorBorder: InputBorder.none,
-            errorBorder: InputBorder.none,
-            enabledBorder: InputBorder.none,
-            focusedBorder: InputBorder.none,
-            suffixIcon: isLoading
-                ? Container(
-                    width: 10,
-                    height: 10,
-                    padding: const EdgeInsets.all(18),
-                    child: const CircularProgressIndicator(
-                      color: Colors.blue,
+        Container(
+          padding: const EdgeInsets.only(left: 10),
+          decoration: showDecoration
+              ? BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: const [
+                    BoxShadow(
+                      blurRadius: 16,
+                      color: Colors.black12,
+                      offset: Offset(0, 4),
                     ),
-                  )
-                : null,
+                  ],
+                )
+              : null,
+          child: FocusScope(
+            onFocusChange: onFocusChange,
+            child: TextFormField(
+              key: controller.fieldKey,
+              validator: validator,
+              readOnly: isLoading,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              controller: controller.textEditingController,
+              onChanged: onChanged,
+              decoration: InputDecoration(
+                hintText: label,
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade400,
+                ),
+                errorText: null,
+                errorStyle: const TextStyle(
+                  color: Colors.transparent,
+                  fontSize: 0,
+                ),
+                focusedErrorBorder: InputBorder.none,
+                errorBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                suffixIcon: isLoading
+                    ? Container(
+                        width: 10,
+                        height: 10,
+                        padding: const EdgeInsets.all(18),
+                        child: const CircularProgressIndicator(
+                          color: Colors.blue,
+                        ),
+                      )
+                    : null,
+              ),
+            ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
